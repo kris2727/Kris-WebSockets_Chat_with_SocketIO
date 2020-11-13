61309348e3f542ea243bcc685c0bdccdc4bed2e0
kris2727 <58188266+kris2727@users.noreply.github.com>
2020-11-12 21:23:19 -0500

    Delete my-function.js

--- my-function.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event) => {
-    // TODO implement
-    var word = event.queryStringParameters.text;
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Kris-Reddivari says  -- '+word),
-    };
-    return response;
-};
